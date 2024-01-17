1943b97f849a689bf584f24572c86197d22b2a76
Nam Phan <phnam181102@gmail.com>
2024-01-17 11:27:01 +0700

    add login and logout features

--- server/routes/user.routes.ts
+++ server/routes/user.routes.ts
@@ -2,12 +2,16 @@ import express from 'express';
 
 import {
     activateUser,
+    loginUser,
+    logoutUser,
     regestrationUser,
 } from '../controllers/users.controller';
 
 const userRouter = express.Router();
 
-userRouter.post('/registration', regestrationUser);
+userRouter.post('/register', regestrationUser);
 userRouter.post('/activate-user', activateUser);
+userRouter.post('/login', loginUser);
+userRouter.get('/logout', logoutUser);
 
 export default userRouter;
