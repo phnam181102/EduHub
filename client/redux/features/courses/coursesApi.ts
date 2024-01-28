cd48d57098981f119c1c1382b7fe7fb575270b50
Phu <vanphu040520022gmail.com>
2024-01-28 23:13:54 +0700

    Fix edit course

--- client/redux/features/courses/coursesApi.ts
+++ client/redux/features/courses/coursesApi.ts
@@ -12,7 +12,7 @@ export const courseApi = apiSlice.injectEndpoints({
     }),
     getAllCourses: builder.query({
       query: () => ({
-        url: "get-courses",
+        url: "get-courses-admin",
         method: "GET",
         credentials: "include" as const,
       }),
